c561e5780cc8bde9eb3f52c5df70a4cda7188c12
gaspardfeuvray <gaspard.feuvray@gmail.com>
2021-08-17 17:59:05 +0200

    feat: add global atom palette

--- reportlib/utils.py
+++ reportlib/utils.py
@@ -1,11 +1,21 @@
 """General utilities"""
 import os
 import webbrowser
+from enum import Enum
+
+
+class Colors(Enum):
+    BLUE = "#636EFA"
+    LIGHTBLUE = "#55BDFF"
+    ORANGE = "#F7C92F"
+    GREEN = "#5DBD48"
+    PINK = "#DE89FA"
+    # px.colors.qualitative.T10[4]
 
 
 def open_documentation():
     """
-    Open the reportlib documentation in new browser tab
+    Open the **reportlib documentation** in a new browser tab
     """
     webbrowser.open(
         "file://" + os.path.abspath(__file__ + "../../../html/reportlib/index.html"),
