08a760e17c6715feee1f6790223eebf7878523e4
gaspardfeuvray <gaspard.feuvray@gmail.com>
2021-08-18 16:41:06 +0200

    adjust palette

--- reportlib/utils.py
+++ reportlib/utils.py
@@ -5,12 +5,12 @@ from enum import Enum
 
 
 class Colors(Enum):
+    LIGHTGREY = "#D0D6D9"
     BLUE = "#636EFA"
     LIGHTBLUE = "#55BDFF"
-    ORANGE = "#F7C92F"
+    ORANGE = "#F2C11F"
     GREEN = "#5DBD48"
     PINK = "#DE89FA"
-    # px.colors.qualitative.T10[4]
 
 
 def open_documentation():
